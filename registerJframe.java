1d3e3420b81587560ac0f23f7b803d5f3f70fc44
catlabor <88519252+catlabor@users.noreply.github.com>
2023-05-29 17:21:10 +0900

    small step

---
+++ registerJframe.java
@@ -0,0 +1,15 @@
+package com.ui;
+
+import javax.swing.*;
+
+public class registerJframe extends JFrame {
+    public registerJframe(){
+        this.setSize(488,500);
+        this.setTitle("registration system");
+        this.setAlwaysOnTop(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        this.setVisible(true);
+    }
+}
---
+++ test.java
@@ -0,0 +1,13 @@
+package com.ui;
+
+import javax.swing.*;
+
+public class test {
+    public static void main(String[] args) {
+        new gameJframe();
+
+        //new loginJframe();
+
+        //new registerJframe();
+    }
+}
